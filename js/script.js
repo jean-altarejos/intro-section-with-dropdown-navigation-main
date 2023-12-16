9d6bb5729c1998273880a691085aa2afa8a1f3b9
jeanaltarejos <jeanaltarejos20@gmail.com>
2023-12-16 22:43:46 +0800

    completed website

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(".navbar-nav li:nth-child(1)").on("click", function(e){
+$(".navbar-nav li:nth-child(1)").on("click", function(){
     $(".feature-toggler").toggleClass("active");
     $(".navbar-nav li:nth-child(1)::after").toggleClass("active");
 });
@@ -7,3 +7,19 @@ $(".navbar-nav li:nth-child(2)").on("click", function(e){
     $(".company-toggler").toggleClass("active");
     $(".navbar-nav li:nth-child(2)::after").toggleClass("active");
 });
+
+$(".navbar-toggler").on("click", function(){
+    $(".sidebar-mobile").addClass("active");
+})
+
+$(".navitem-feat").on("click", function(){
+    $(".feat-menu").toggleClass("active");
+})
+
+$(".navitem-company").on("click", function(){
+    $(".company-menu").toggleClass("active");
+})
+
+$(".cancel-btn").on("click", function(){
+    $(".sidebar-mobile").removeClass("active");
+})
